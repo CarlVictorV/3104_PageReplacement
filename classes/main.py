682515e230e30b82cb7ac12fad82ea723fced733
CarlVictorV <19104214@usc.edu.ph>
2023-11-24 08:52:01 +0800

    fixed menu

--- classes/main.py
+++ classes/main.py
@@ -6,12 +6,15 @@ import input_taker as i
 import os
 
 test = i.SequenceTaker("temp")
+
+
 def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def menu():
-    print("MENU: ")
-    print()
+    print('+-------------------------------------+')
+    print('|--- *Page Replacement Simulation* ---|')
+    print('+-------------------------------------+\n')
     print("[1] FIFO")
     print("[2] LRU")
     print("[3] LFU")
@@ -20,64 +23,48 @@ def menu():
     print()
 
 
-def fifo():
+def execute_algorithm(algorithm):
     if os.name != 'nt':
-        test.retake_sequence(r"inputs/fifo.txt")
+        test.retake_sequence(f"inputs/{algorithm.lower()}.txt")
     else:
-        test.retake_sequence(r"inputs\fifo.txt")
-    fifo = f.FIFO(test.sequence, test.frames, test.process_count)
+        test.retake_sequence(f"inputs\\{algorithm.lower()}.txt")
 
-
-def lru():
-    if os.name != 'nt':
-        test.retake_sequence(r"inputs/lru.txt")
-    else:
-        test.retake_sequence(r"inputs\lru.txt")
-    lru = lr.LRU(test.sequence, test.frames, test.process_count)
-
-
-def lfu():
-    if os.name != 'nt':
-        test.retake_sequence(r"inputs/lfu.txt")
-    else:
-        test.retake_sequence(r"inputs\lfu.txt")
-    lfu = lf.LFU(test.sequence, test.frames, test.process_count)
-
-
-def optimal():
-    if os.name != 'nt':
-        test.retake_sequence(r"inputs/optimal.txt")
-    else:
-        test.retake_sequence(r"inputs\optimal.txt")
-    opt = o.Optimal(test.sequence, test.frames, test.process_count)
+    if algorithm == "FIFO":
+        f.FIFO(test.sequence, test.frames, test.process_count)
+    elif algorithm == "LRU":
+        lr.LRU(test.sequence, test.frames, test.process_count)
+    elif algorithm == "LFU":
+        lf.LFU(test.sequence, test.frames, test.process_count)
+    elif algorithm == "Optimal":
+        o.Optimal(test.sequence, test.frames, test.process_count)
 
 
 def main():
-    while (True):
+    algorithms = {
+        "1": "FIFO",
+        "2": "LRU",
+        "3": "LFU",
+        "4": "Optimal"
+    }
+
+    while True:
         menu()
         choice = input("Enter choice: ")
         print()
-        if choice == "1":
-            clear()
-            fifo()
-        elif choice == "2":
-            clear()
-            lru()
-        elif choice == "3":
-            clear()
-            lfu()
-        elif choice == "4":
-            clear()
-            optimal()
-        elif choice == "0":
+
+        if choice == "0":
             clear()
             print("Exiting...")
             return
+
+        algorithm = algorithms.get(choice)
+        if algorithm:
+            clear()
+            execute_algorithm(algorithm)
         else:
             clear()
             print("Invalid input. Please try again.")
             print()
-            continue
 
 
 if __name__ == "__main__":
